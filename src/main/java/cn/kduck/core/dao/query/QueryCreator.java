5480dfe1687d9d5d477fb4481f1536da27f374ab
刘海刚 <lhg_0317@163.com>
2022-01-29 19:59:27 +0800

    QueryCreator默认生成的Bean名称由类名改为全路径

--- src/main/java/cn/kduck/core/dao/query/QueryCreator.java
+++ src/main/java/cn/kduck/core/dao/query/QueryCreator.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public interface QueryCreator {
 
     default String queryCode(){
-        return getClass().getSimpleName();
+        return getClass().getName();
     }
 
     /**
